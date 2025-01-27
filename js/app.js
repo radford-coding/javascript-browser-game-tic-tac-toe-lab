73fe73c2c0a7ba7868ea49adec37b4fcfc73f128
Matt Radford <quinquex@gmail.com>
2025-01-27 14:23:18 -0800

    further tweaks, add level-up content

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ const resetBtnEl = document.querySelector('#reset');
 function init() {
     // create an array of n=9 empty strings
     board = [...Array(9)].map(() => '');
-    turn = 'X';
+    turn = player1;
     winner = false;
     tie = false;
     render();
@@ -41,19 +41,15 @@ function render() {
 
 // display the board state for the user
 function updateBoard() {
-    // not sure how to use forEach() here, as board and squareEls aren't related except by index position?
-    for (i = 0; i < board.length; i++) {
-        squareEls[i].innerText = board[i];
-    };
-
+    squareEls.forEach(s => s.innerText = board[s.id]);
 };
 
 // appropriately display text for the user
 function updateMessage() {
     if (!winner && !tie) {
-        messageEl.innerText = `it's ${turn}'s turn`;
+        messageEl.innerText = `${turn}'s turn`;
     } else if (!winner && tie) {
-        messageEl.innerText = 'wow what a riveting game. try again nerds';
+        messageEl.innerText = 'wow what a riveting game. try again';
     } else {
         messageEl.innerText = `${turn} wins!`;
     };
@@ -61,13 +57,12 @@ function updateMessage() {
 
 // logic for handling a click event e
 function handleClick(e) {
-    // console.log('ow! ' + e.target.id);
     const squareIndex = e.target.id;
     if (board[squareIndex] !== '') { // square already occupied
         return;
     } else if (winner) { // game over
         return;
-    } else {
+    } else { // game logic
         placePiece(squareIndex);
         checkForWinner();
         checkForTie();
@@ -103,15 +98,15 @@ function checkForTie() {
     };
 };
 
-// alternate turns. sharing is caring.
+// alternate turns. sharing is caring folks.
 function switchPlayerTurn() {
     if (winner) {
         return;
     } else {
-        if (turn === 'X') {
-            turn = 'O';
+        if (turn === player1) { // now x/o agnostic
+            turn = player2;
         } else {
-            turn = 'X';
+            turn = player1;
         };
     };
 };
@@ -127,9 +122,58 @@ theBoard.addEventListener('click', (e) => {
 });
 
 resetBtnEl.addEventListener('click', () => {
-    init();
+    choose();
 });
 
 /*----------------------------- initialize ----------------------------------*/
 
-init();
+// init(); // moved to after the symbol selection
+
+/*----------------------------- level up content ----------------------------*/
+
+// cached reference to the available icon choices
+const choiceEls = document.querySelectorAll('.choice'); // clickables
+const choosing = document.querySelector('.choosing'); // choice elements
+const choiceMessage = document.querySelector('#choice-message');
+
+// variables for the player icons and counter
+let player1, player2;
+let playerNum = 1;
+
+// functions
+
+function handleChoice(e) {
+    if (playerNum === 1) {
+        player1 = e.target.innerText;
+        e.target.style.color = 'black'; // sort of hide it
+        playerNum += 1;
+        choose();
+    } else if (playerNum === 2) {
+        player2 = e.target.innerText;
+        choosing.style.display = 'none';
+        theBoard.style.display = 'flex';
+        playerNum = 1;
+        choiceEls.forEach(c => c.style.color = 'white'); // sort of unhide it
+        init();
+    };
+};
+
+
+function choose() {
+    messageEl.innerText = '';
+    theBoard.style.display = 'none';
+    choosing.style.display = 'flex';
+    choiceMessage.innerText = `Please select an icon for player ${playerNum}`;
+};
+
+choose();
+
+
+// event listeners for the initial two icon choices
+choiceEls.forEach(c => c.addEventListener('click', handleChoice));
+
+
+// prompt user to make a choice for player1, player2
+// hide choices and show the game board
+// use those choices for the game
+// reset() returns to piece selection
